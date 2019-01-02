ba4682865e9cb41f74a41937a431830cdee0ae96
akharlov <dronindigo@gmail.com>
2019-01-02 13:25:39 +0300

    Task 01 from the homework 01 is completed.

--- JavaCoreDec2018/src/com/epam/javacore/homework01/task01/logic/Logic.java
+++ JavaCoreDec2018/src/com/epam/javacore/homework01/task01/logic/Logic.java
@@ -1,4 +1,7 @@
 package com.epam.javacore.homework01.task01.logic;
 
 public class Logic {
+    public String method(){
+        return "I am string in Logic.";
+    }
 }
--- JavaCoreDec2018/src/com/epam/javacore/homework01/task01/main/Main.java
+++ JavaCoreDec2018/src/com/epam/javacore/homework01/task01/main/Main.java
@@ -1,4 +1,11 @@
 package com.epam.javacore.homework01.task01.main;
 
+import com.epam.javacore.homework01.task01.logic.Logic;
+
 public class Main {
+
+    public static void main(String[] args){
+        Logic logic = new Logic();
+        System.out.println(logic.method());
+    }
 }
