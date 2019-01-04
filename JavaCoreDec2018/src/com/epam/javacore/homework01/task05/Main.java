bcc7082c86583a486906316a1c5ab314f837ffd4
akharlov <dronindigo@gmail.com>
2019-01-04 12:42:03 +0300

    Initial commit. Homework 01, task 05.

---
+++ JavaCoreDec2018/src/com/epam/javacore/homework01/task05/Main.java
@@ -0,0 +1,55 @@
+package com.epam.javacore.homework01.task05;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        int arraySize = 0;
+
+        while (true){
+            System.out.print("Please enter a size for the array: ");
+            Scanner scanner = new Scanner(System.in);
+            String number = scanner.nextLine();
+
+            try {
+                arraySize = Integer.parseInt(number);
+            } catch (NumberFormatException ex){
+                System.out.println("Please use only integer numbers.\n");
+            }
+
+            if (arraySize == 0){
+                System.out.println("A size for the array can not be 0.\n");
+            } else if (arraySize % 2 == 0){
+                System.out.println("Please use only odd numbers\n");
+            } else {
+                break;
+            }
+        }
+
+        Main main = new Main();
+        int[][] array = main.createArray(arraySize);
+
+        for (int i = 0; i < arraySize; i++){
+            for (int j = 0; j < arraySize; j++){
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private int[][] createArray(int arraySize){
+        int[][] array = new int[arraySize][arraySize];
+
+        for (int i = 0; i < arraySize; i++){
+            for (int j = 0; j < arraySize; j++){
+                if (i == j || arraySize - i == j + 1){
+                    array[i][j] = 1;
+                } else {
+                    array[i][j] = 0;
+                }
+            }
+        }
+
+        return array;
+    }
+}
