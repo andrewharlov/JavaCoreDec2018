2d41f1fa2bc0aae7061e20a00affe35a5a054681
akharlov <dronindigo@gmail.com>
2019-01-07 21:22:20 +0300

    Homework 01, task 06. Added Javadoc for the class 'Note'.

--- JavaCoreDec2018/src/com/epam/javacore/homework01/task06/Note.java
+++ JavaCoreDec2018/src/com/epam/javacore/homework01/task06/Note.java
@@ -1,16 +1,27 @@
 package com.epam.javacore.homework01.task06;
 
+/**
+ * This class represents a note in a notebook
+ * @author Andrei Kharlov
+ * @version 1.0
+ */
 public class Note {
+    /** text String This is a text of a note*/
     public String text;
 
+    /**Class constructor for a note
+     * @param text String This is a text for a note*/
     public Note(String text) {
         this.text = text;
     }
-
+    /**Returns a text of a note
+     * @return String This is a text a note*/
     public String getText() {
         return this.text;
     }
 
+    /**Checks whether some other object is "equal to" this one
+     * @return boolean True if two objects have the same value in the field 'text'*/
     @Override
     public boolean equals(Object obj) {
         if (obj == null) {
