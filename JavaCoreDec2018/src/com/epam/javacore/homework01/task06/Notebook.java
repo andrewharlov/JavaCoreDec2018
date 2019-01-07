3f4df856e1700a2bbc729aeb53d47684d99f1c04
akharlov <dronindigo@gmail.com>
2019-01-07 21:54:03 +0300

    Homework 01, task 06. Added Javadoc for the class 'Notebook'.

--- JavaCoreDec2018/src/com/epam/javacore/homework01/task06/Notebook.java
+++ JavaCoreDec2018/src/com/epam/javacore/homework01/task06/Notebook.java
@@ -4,17 +4,35 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * This class represents a notebook
+ * @author Andrei Kharlov
+ * @version 1.0
+ */
 public class Notebook {
+    /**notes List<Note> This is a list that represents notes in a notebook */
     public List<Note> notes;
 
+    /**
+     * Constructor for a notebook
+     */
     public Notebook(){
         this.notes = new ArrayList<>();
     }
 
+    /**
+     * Adds a note to a notebook
+     * @param note Note This is a note to add
+     */
     public void addNote(Note note){
         this.notes.add(note);
     }
 
+    /**
+     * Changes a note in a notebook
+     * @param noteToEdit Note This is a note to edit
+     * @param newText String This is a new text for the note
+     */
     public void editNote(Note noteToEdit, String newText){
         for (Note currentNote : this.notes){
             if (currentNote.equals(noteToEdit)){
@@ -25,6 +43,10 @@ public class Notebook {
         }
     }
 
+    /**
+     * Deletes a note from a notebook
+     * @param noteToDelete Note This is a note to delete
+     */
     public void deleteNote(Note noteToDelete){
         Iterator<Note> iterator = this.notes.iterator();
 
@@ -36,6 +58,10 @@ public class Notebook {
         }
     }
 
+    /**
+     * Returns all notes from a notebook in a formatted string
+     * @return String This is a string with all notes from a notebook
+     */
     public String printNotes(){
         StringBuilder notesToPrint = new StringBuilder("");
 
