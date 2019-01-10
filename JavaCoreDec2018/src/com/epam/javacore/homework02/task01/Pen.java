af6747ce05e9bd63cb34616628a58219f6714f10
akharlov <dronindigo@gmail.com>
2019-01-10 14:03:41 +0300

    Homework 02, task 01. Initial commit.

--- JavaCoreDec2018/src/com/epam/javacore/homework02/task01/Pen.java
+++ JavaCoreDec2018/src/com/epam/javacore/homework02/task01/Pen.java
@@ -1,4 +1,40 @@
 package com.epam.javacore.homework02.task01;
 
 public class Pen {
+    private String color;
+    private int width;
+
+    public Pen(String color, int width){
+        this.color = color;
+        this.width = width;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = this.color.hashCode() ^ (this.color.hashCode() >>> 11);
+        result = result ^ (59 * this.width);
+        return  result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null){
+            return false;
+        }
+
+        if (obj instanceof Pen){
+            Pen otherPen = (Pen) obj;
+
+            if (this.color.equals(otherPen.color) && this.width == otherPen.width){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Pen has " + this.color + " and " + this.width + " width.";
+    }
 }
