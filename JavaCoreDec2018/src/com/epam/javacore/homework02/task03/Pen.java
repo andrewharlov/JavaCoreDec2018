fe420c72f3d0ebd2de966782a3c404a5185fff2d
akharlov <dronindigo@gmail.com>
2019-01-11 17:16:07 +0300

    Homework 02, task 03. Changed implementation of the class 'Pen'.

--- JavaCoreDec2018/src/com/epam/javacore/homework02/task03/Pen.java
+++ JavaCoreDec2018/src/com/epam/javacore/homework02/task03/Pen.java
@@ -1,12 +1,30 @@
 package com.epam.javacore.homework02.task03;
 
 public class Pen extends DeskItem {
+    private static final String type = "Pen";
     private String color;
     private int width;
 
     public Pen(String color, int width, String brandName, double price){
-        super(brandName, price);
+        super(type, brandName, price);
         this.color = color;
         this.width = width;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    @Override
+    public String toString() {
+        return "Type: " + getType() + "; " +
+                "Brand: " + getBrand() + "; " +
+                "Price: " + getPrice() + " " +
+                "Color: " + this.getColor() + "; " +
+                "Width: " + this.getWidth() + "\n";
     }
 }
