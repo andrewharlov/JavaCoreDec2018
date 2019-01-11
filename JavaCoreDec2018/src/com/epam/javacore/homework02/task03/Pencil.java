42c6cfe76273f360c4f5228311995795429c28cd
akharlov <dronindigo@gmail.com>
2019-01-11 17:16:28 +0300

    Homework 02, task 03. Changed implementation of the class 'Pencil'.

--- JavaCoreDec2018/src/com/epam/javacore/homework02/task03/Pencil.java
+++ JavaCoreDec2018/src/com/epam/javacore/homework02/task03/Pencil.java
@@ -1,10 +1,23 @@
 package com.epam.javacore.homework02.task03;
 
 public class Pencil extends DeskItem {
+    private static final String type = "Pencil";
     private String hardness;
 
     public Pencil(String hardness, String brandName, double price){
-        super(brandName, price);
+        super(type, brandName, price);
         this.hardness = hardness;
+    }
+
+    public String getHardness() {
+        return hardness;
+    }
+
+    @Override
+    public String toString() {
+        return "Type: " + getType() + "; " +
+                "Brand: " + getBrand() + "; " +
+                "Price: " + getPrice() + " " +
+                "Hardness: " + this.getHardness() + "\n";
     }
 }
