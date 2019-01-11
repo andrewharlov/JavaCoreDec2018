9f278a5d213a8d28eaf66fc30c9848704a98c107
akharlov <dronindigo@gmail.com>
2019-01-11 17:17:30 +0300

    Homework 02, task 03. Changed implementation of the class 'Scissors'.

--- JavaCoreDec2018/src/com/epam/javacore/homework02/task03/Scissors.java
+++ JavaCoreDec2018/src/com/epam/javacore/homework02/task03/Scissors.java
@@ -1,10 +1,23 @@
 package com.epam.javacore.homework02.task03;
 
 public class Scissors extends DeskItem {
+    private static final String type = "Scissors";
     private int size;
 
     public Scissors(int size, String brandName, double price){
-        super(brandName, price);
+        super(type, brandName, price);
         this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return "Type: " + getType() + "; " +
+                "Brand: " + getBrand() + "; " +
+                "Price: " + getPrice() + " " +
+                "Length: " + this.getSize() + "\n";
     }
 }
