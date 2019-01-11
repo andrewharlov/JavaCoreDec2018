d90f3ddb5bf638d0dc057d485474c6b616daf210
akharlov <dronindigo@gmail.com>
2019-01-11 17:16:46 +0300

    Homework 02, task 03. Changed implementation of the class 'Ruler'.

--- JavaCoreDec2018/src/com/epam/javacore/homework02/task03/Ruler.java
+++ JavaCoreDec2018/src/com/epam/javacore/homework02/task03/Ruler.java
@@ -1,10 +1,23 @@
 package com.epam.javacore.homework02.task03;
 
 public class Ruler extends DeskItem {
+    private static final String type = "Ruler";
     private int length;
 
     public Ruler(int length, String brandName, double price){
-        super(brandName, price);
+        super(type, brandName, price);
         this.length = length;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    @Override
+    public String toString() {
+        return "Type: " + getType() + "; " +
+                "Brand: " + getBrand() + "; " +
+                "Price: " + getPrice() + " " +
+                "Length: " + this.getLength() + "\n";
     }
 }
