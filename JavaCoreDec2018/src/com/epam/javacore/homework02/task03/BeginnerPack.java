fcec48d3343487a05498fe08e9938021f79931cd
akharlov <dronindigo@gmail.com>
2019-01-14 13:30:13 +0300

    Homework 02, task 04. Created method 'deskItemsToString' for the class 'BeginnerPack' and implementation of the interface 'Sort'.

--- JavaCoreDec2018/src/com/epam/javacore/homework02/task03/BeginnerPack.java
+++ JavaCoreDec2018/src/com/epam/javacore/homework02/task03/BeginnerPack.java
@@ -1,9 +1,10 @@
 package com.epam.javacore.homework02.task03;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.epam.javacore.homework02.task04.Sort;
 
-public class BeginnerPack {
+import java.util.*;
+
+public class BeginnerPack implements Sort {
     private List<DeskItem> deskItems;
 
     public BeginnerPack(){
@@ -16,5 +17,52 @@ public class BeginnerPack {
 
     public void addDeskItem(DeskItem deskItem){
         this.deskItems.add(deskItem);
+    }
+
+    @Override
+    public void sortByPrice() {
+        Collections.sort(this.deskItems, new Comparator<DeskItem>() {
+            @Override
+            public int compare(DeskItem item1, DeskItem item2) {
+                if (item1.getPrice() == item2.getPrice()){
+                    return 0;
+                }
+                return item1.getPrice() < item2.getPrice() ? -1 : 1;
+            }
+        });
+    }
+
+    @Override
+    public void sortByName() {
+        Collections.sort(this.deskItems, Comparator.comparing(DeskItem::getBrand));
+    }
+
+    @Override
+    public void sortByNameAndPrice() {
+        Collections.sort(this.deskItems, new Comparator<DeskItem>() {
+            @Override
+            public int compare(DeskItem item1, DeskItem item2) {
+                int result1 = item1.getBrand().compareTo(item2.getBrand());
+
+                if (result1 == 0){
+                    if (item1.getPrice() == item2.getPrice()){
+                        return 0;
+                    }
+                    return item1.getPrice() < item2.getPrice() ? -1 : 1;
+                }
+
+                return result1;
+            }
+        });
+    }
+
+    public String deskItemsToString(){
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (DeskItem item : this.deskItems){
+            stringBuilder.append(item.toString());
+        }
+
+        return stringBuilder.toString();
     }
 }
