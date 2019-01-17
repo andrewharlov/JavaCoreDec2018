04791459a732144f95871d25c09c638fb9b95405
akharlov <dronindigo@gmail.com>
2019-01-17 22:56:06 +0300

    Homework 02, task 07. Added Javadoc annotations for the class 'Ship'.

--- JavaCoreDec2018/src/com/epam/javacore/homework02/task06/Ship.java
+++ JavaCoreDec2018/src/com/epam/javacore/homework02/task06/Ship.java
@@ -1,32 +1,68 @@
 package com.epam.javacore.homework02.task06;
 
+/**
+ * This class represents a ship
+ * @author Andrei Kharlov
+ * @version 1.0
+ */
 public class Ship {
+    /**name String This is a String that holds a name of a ship*/
     private String name;
 
+    /**
+     * Constructor for a ship
+     * @param name This is a name of a ship
+     */
     public Ship(String name) {
         this.name = name;
     }
 
+    /**
+     * Launches a ship
+     * @return String This is a message from a running engine
+     */
     public String launch(){
         Ship.Engine engine = this.new Engine(450);
         return engine.startEngine();
     }
 
+    /**
+     * Returns a ship's name
+     * @return String This is a name of a ship
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * This class represents a ship's engine
+     * @author Andrei Kharlov
+     * @version 1.0
+     */
     class Engine{
+        /**horsepower int This is an int that holds a horsepower number*/
         private int horsepower;
 
+        /**
+         * Constructor for an engine
+         * @param horsepower This is an engine's amount of horsepower
+         */
         public Engine(int horsepower) {
             this.horsepower = horsepower;
         }
 
+        /**
+         * Starts an engine
+         * @return String This is a message from a running engine
+         */
         public String startEngine(){
             return Ship.this.getName() + " puts out " + this.getHorsepower() + " horsepower ...";
         }
 
+        /**
+         * Returns a horsepower of an engine
+         * @return int This is a horsepower of an engine
+         */
         public int getHorsepower() {
             return horsepower;
         }
